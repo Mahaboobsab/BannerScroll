9d2dd35e2a763a722efc2d79b9cf585a6b35c143
mahaboobsab <mrnadaf2010@gmail.com>
2019-02-03 17:00:04 +0530

    Initial commit

--- BannerPod/ViewController.swift
+++ BannerPod/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import BannerView
+class ViewController: UIViewController,BannerViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var topBannerView: BannerView!
+    
+    let items: [BannerItem] = [
+        BannerItem.init(image: UIImage.init(named: "Banner1.jpg")),
+        BannerItem.init(image: UIImage.init(named: "Banner2.jpg")),
+        BannerItem.init(image: UIImage.init(named: "Banner3.jpg"))
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        topBannerView.setup(
+            type: BannerViewScrollType.fromStart,
+            timeForOneItem: 2,
+            bannerItems: items,
+            delegate: self
+        )
+        topBannerView.pageControl.color = UIColor.white
+        topBannerView.pageControl.currentPageColor = .white
+        
+        
     }
 
 
